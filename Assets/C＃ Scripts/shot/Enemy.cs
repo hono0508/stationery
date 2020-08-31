a8b937116df0c9a8adfcf3860c7ee6aa24a1d8a9
hono0508 <pocky16141@gmail.com>
2020-08-31 17:46:25 +0900

    bunbougu

--- Assets/C＃ Scripts/shot/Enemy.cs
+++ Assets/C＃ Scripts/shot/Enemy.cs
@@ -6,8 +6,11 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-
+    float interval = 5.0f;
+    float timer = 0;
     public GameObject enemy;
+    public GameObject[] enemyLooks;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,9 @@ public class Enemy : MonoBehaviour
 
         for (int e = 1; e <= 4; e++)
         {
-            Instantiate(enemy, new Vector3(Random.Range(10.0f,-2.5f), 12,0), Quaternion.identity);
-
-            GameObject player = GameObject.Find("ThirdPersonController");
+            int enemyL = Random.Range(0, enemyLooks.Length); //見た目をランダムにする
+            Transform ene = Instantiate(enemy, new Vector3(Random.Range(10.0f, -2.5f), -1, 0), Quaternion.identity).transform;
+            Instantiate(enemyLooks[enemyL], ene.position, Quaternion.identity, ene);
 
         }
     }
@@ -28,8 +31,21 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        if (timer > interval)
+        {
+            float posX = Random.Range(-16, 27); //位置をランダムにする
+            float posZ = Random.Range(-10, 17); //位置をランダムにする
+            int enemyL = Random.Range(0, enemyLooks.Length); //見た目をランダムにする
+
+            Transform e = Instantiate(enemy, new Vector3(posX, -1, posZ), Quaternion.identity).transform;
+            Instantiate(enemyLooks[enemyL], e.position, Quaternion.identity, e);
+
+            timer = 0; //タイマーをリセットする
+        }
     
-    }
+}
 }
     
 
